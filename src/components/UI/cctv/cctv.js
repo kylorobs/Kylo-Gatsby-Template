1654038324586cb13d27c2821af79ebf018ac1af
KCLSU <45687039+KCLSU@users.noreply.github.com>
2020-06-29 13:56:49 +0200

    cctv styled component

--- src/components/UI/cctv/cctv.js
+++ src/components/UI/cctv/cctv.js
@@ -1,7 +1,31 @@
 import React from 'react';
-import { useStaticQuery, graphql } from "gatsby"
-import classes from './cctvStyles.module.css';
+import { useStaticQuery, graphql } from "gatsby";
+import styled, { keyframes } from "styled-components";
 
+const slideRight = keyframes`
+  0% {
+    transform: translateX(1000px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+const SlideIn = styled.div`
+  position: absolute;
+  width: 150px;
+  opacity: 0;
+  top: 0;
+  right: 5px;
+  animation: ${slideRight} 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) 0s forwards;
+  
+  @media (max-width: 1000px){
+    width: 100px;
+    right: 5px;
+  }
+`
 
 const CCTV = props => {
     const data = useStaticQuery(graphql`
@@ -19,9 +43,9 @@ const CCTV = props => {
   `)
 
   return (
-    <div className={classes.CCTV}>
+    <SlideIn>
         <img src={data.placeholderImage.publicURL} alt="CCTV" />
-    </div>
+    </SlideIn>
   )
 }
 
